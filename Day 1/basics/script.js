3da9167e9915e4e9d6835f07bbc02402d5237f28
noodles-dev <simi.simecek@seznam.cz>
2022-12-16 17:14:38 +0100

    Add Day 1 + Template

---
+++ Day 1/basics/script.js
@@ -0,0 +1,16 @@
+//Hello World but little more complex
+var Hello, World;                     
+
+Hello = "Hello";
+World = "World";
+
+console.log(Hello + " " + World);
+
+// numbers
+var Apples, Pears, Fruit;
+
+Apples = 3;
+Pears = 2;
+Fruit = Apples + Pears
+
+console.log(Fruit)
