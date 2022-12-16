69e7c9c58e7fa6c8ef13635d4e3a8b07f84fdc56
noodles-dev <simi.simecek@seznam.cz>
2022-12-16 19:36:01 +0100

    Finish greeting

--- Day 1/greeting/script.js
+++ Day 1/greeting/script.js
@@ -5,7 +5,31 @@ d = new Date();
 hours = d.getHours();
 minutes = d.getMinutes();
 
-// hours has two numbers
+// hours always has two digits
 if (hours < 10) {
-    hours
+    hours = "0" + hours;
+}   else {
+    hours = hours.toString();
 }
+// minutes always has two digits
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}   else {
+    minutes = minutes.toString();
+}
+// connecting hours minutes to a four digit numberS
+time = Number(hours + minutes);
+
+if (time >= 0000 && time < 1200) {
+    message = "Good morning!";
+} else if (time >= 1200 && time < 1300) {
+    message = "Good noon!";
+} else if (time >= 1300 && time < 1700) {
+    message = "Good afternoon!";
+} else if (time >= 1700 && time < 2200) {
+    message = "Good evening!";
+} else if (time >= 2200 && time < 2359) {
+    message = "Good night!";
+}
+
+alert(message);
